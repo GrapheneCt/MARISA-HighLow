c5648533dbc4c23e61bba01f73a234f3f30675f0
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-07-24 18:50:48 +0200

    Use latest version of libvitaSAS

--- src/sound.c
+++ src/sound.c
@@ -3,7 +3,6 @@
 #include <psp2/kernel/clib.h> 
 #include <psp2/io/fcntl.h> 
 #include <psp2/types.h> 
-#include <psp2/sas.h> 
 #include <psp2/audioout.h>  
 #include <psp2/kernel/threadmgr.h> 
 
@@ -19,17 +18,39 @@ extern SceUInt8 g_global_state;
 SceVoid soundInitialize(SceVoid)
 {
 	vitaSAS_pass_mspace(g_mspace);
-	vitaSAS_init(SCE_AUDIO_OUT_PORT_TYPE_MAIN, 48000, 256, 64, 128 * 1024, 0, 0);
+	vitaSAS_init(0);
+
+	VitaSASSystemParam initParam;
+	initParam.outputPort = SCE_AUDIO_OUT_PORT_TYPE_MAIN;
+	initParam.samplingRate = 48000;
+	initParam.numGrain = 256;
+	initParam.thPriority = 64;
+	initParam.thStackSize = 128 * 1024;
+	initParam.thCpu = 0;
+	initParam.isSubSystem = SCE_FALSE;
+	initParam.subSystemNum = VITASAS_NO_SUBSYSTEM;
+
+	vitaSAS_create_system_with_config("numGrains=256 numVoices=12 numReverbs=0", &initParam);
+
 	SceChar8 path[23];
+
+	vitaSASVoiceParam voiceParam;
+	voiceParam.loop = 0;
+	voiceParam.loopSize = 0;
+	voiceParam.pitch = 4096;
+	voiceParam.volLDry = 4096;
+	voiceParam.volRDry = 4096;
+	voiceParam.volLWet = 0;
+	voiceParam.volRWet = 0;
+	voiceParam.adsr1 = 0;
+	voiceParam.adsr2 = 0;
 
 	for (int i = 0; i < 11; i++) {
 		sceClibSnprintf(path, 23, "/gamedata/sound/%d.vag", i);
 		DEBUG_PRINT("loading: %s\n", path);
-		vitaSAS_set_voice_VAG(i, vitaSAS_load_audio_VAG(path, 1), SCE_SAS_LOOP_DISABLE, 4096, 4096, 4096, 0, 0, 0, 0);
+		vitaSAS_set_voice_VAG(i, vitaSAS_load_audio_VAG(path, 1), &voiceParam);
 		g_loading++;
 	}
-
-	//vitaSAS_set_voice_VAG(0, audioBlock[3], SCE_SAS_LOOP_DISABLE, 4096, 4096, 4096, 0, 0, 0, 0);
 }
 
 SceInt32 soundMain(SceSize argc, ScePVoid argv)
@@ -39,73 +60,18 @@ SceInt32 soundMain(SceSize argc, ScePVoid argv)
 
 	while (1) {
 
-
 		if (heartbeat_counter == 0 && g_global_state != 4 && g_global_state != 7 && g_global_state != 8)
-			sceSasSetKeyOn(3);
+			vitaSAS_set_key_on(3);
 
-		if (subvoice_id == -1) {
-			switch (g_audio_sample) {
-			case 0:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 0;
-				break;
-			case 1:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 1;
-				break;
-			case 2:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 2;
-				break;
-			case 3:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 3;
-				break;
-			case 4:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 4;
-				break;
-			case 5:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 5;
-				break;
-			case 6:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 6;
-				break;
-			case 7:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 7;
-				break;
-			case 8:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 8;
-				break;
-			case 9:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 9;
-				break;
-			case 10:
-				sceSasSetKeyOn(g_audio_sample);
-				g_audio_sample = 11;
-				subvoice_id = 10;
-				break;
-			}
+		if (subvoice_id == -1 && g_audio_sample < 11) {
+			vitaSAS_set_key_on(g_audio_sample);
+			subvoice_id = g_audio_sample;
+			g_audio_sample = 11;
 		}
 
 		if (subvoice_id > -1)
-			if (sceSasGetEndState(subvoice_id)) {
-				sceSasSetKeyOff(subvoice_id);
+			if (vitaSAS_get_end_state(subvoice_id)) {
+				vitaSAS_set_key_off(subvoice_id);
 				subvoice_id = -1;
 			}
 
@@ -113,7 +79,7 @@ SceInt32 soundMain(SceSize argc, ScePVoid argv)
 			heartbeat_counter++;
 		else {
 			heartbeat_counter = 0;
-			sceSasSetKeyOff(3);
+			vitaSAS_set_key_off(3);
 		}
 
 		sceKernelDelayThread(30000);
